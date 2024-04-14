305261d7b9df2944ab9211938fdac217004a1f95
shamshaev <an.shamshaev@gmail.com>
2024-04-15 00:41:36 +0300

    Change MapSchema

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -3,7 +3,7 @@ package hexlet.code.schemas;
 import java.util.HashMap;
 import java.util.Map;
 
-public final class MapSchema<T> extends BaseSchema<Map<?, T>> {
+public final class MapSchema<T> extends BaseSchema<Map<?, ?>> {
     private boolean isSizeOfActive;
     private int sizeConstraint;
     private boolean isShapeActive;
@@ -26,21 +26,28 @@ public final class MapSchema<T> extends BaseSchema<Map<?, T>> {
         return this;
     }
 
-    private boolean isSizeofValid(Map<?, T> map) {
+    private boolean isSizeofValid(Map<?, ?> map) {
         return !isSizeOfActive || map.size() == sizeConstraint;
     }
 
-    private boolean isShapeValid(Map<?, T> map) {
+    @SuppressWarnings("unchecked")
+    private boolean isShapeValid(Map<?, ?> map) {
         return !isShapeActive || map.size() == map.keySet().stream()
                 .filter(key -> {
                     var schema = mapConstraint.get(key);
-                    return schema.isValid(map.get(key));
+                    T valueMap;
+                    try {
+                        valueMap = (T) map.get(key);
+                    } catch (ClassCastException e) {
+                        throw new ClassCastException();
+                    }
+                    return schema.isValid(valueMap);
                 })
                 .count();
     }
 
     @Override
-    public boolean isValid(Map<?, T> map) {
+    public boolean isValid(Map<?, ?> map) {
         var condition1 = isRequiredValidWithNull(map);
         var condition2 = isRequiredValidWithNotNull(map) && isSizeofValid(map) && isShapeValid(map);
 
